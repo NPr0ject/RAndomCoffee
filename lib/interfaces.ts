27e8e9c20a1a128f191e251ed967cbd4176ddbb2
NPr0ject <iwanow32855@gmail.com>
2024-12-09 08:28:44 +0300

    Update interfaces.ts

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,10 +1,17 @@
+import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
+
 export interface UserInfo {
   id: number;
   name: string;
   age: number;
   interests: string[];
-  time: number;
-  coffee: number;
-  state: string;
+  coffee:string;
   done: boolean;
+  state: string;
 }
+
+
+
+export type MyContext = Context & {
+  config: UserInfo;
+};
